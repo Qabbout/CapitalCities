ca715848cfc0cc77bc8961cab47280aa4e05d696
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-13 14:02:21 +0300

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Abdulrahman on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
